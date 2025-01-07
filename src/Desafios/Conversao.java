c4fe7d3e67d087a4d0617229c24eb5de6ea18ff7
Bruno <2411550255@fema.edu.br>
2025-01-07 16:02:36 -0300

    Primeiro exercicio de conversão: Erros: Para que seja possivel digitar tanto vilgula quanto ponto, deve-se incerir um replace, para que toda vez que apareca uma virgular, ela seja substituida por ponto!

---
+++ src/Desafios/Conversao.java
@@ -0,0 +1,24 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class Conversao {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu 1º salário: ");
+        String salario1 = sc.nextLine().replace(",", ".");
+        System.out.println("Digite seu 2º salário: ");
+        String salario2 = sc.nextLine().replace(",", ".");
+        System.out.println("Digite seu 3º salário: ");
+        String salario3 = sc.nextLine().replace(",", ".");
+
+        Double valorSalario1 = Double.parseDouble(salario1);
+        Double valorSalario2 = Double.parseDouble(salario2);
+        Double valorSalario3 = Double.parseDouble(salario3);
+
+        System.out.println("Média salárial: " + (valorSalario1 + valorSalario2 + valorSalario3)/3);
+        sc.close();
+    }
+}
