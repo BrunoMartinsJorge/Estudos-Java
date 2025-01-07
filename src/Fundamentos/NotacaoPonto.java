c30d7e8ec0a6d251b4b9145352757f2a80e1083d
Bruno <2411550255@fema.edu.br>
2025-01-07 00:36:51 -0300

    Fix: Iniciando meus estudos em Java. Tipos de variaveis, e alguns exercicios!

---
+++ src/Fundamentos/NotacaoPonto.java
@@ -0,0 +1,24 @@
+package Fundamentos;
+
+public class NotacaoPonto {
+
+    public static void main(String[] args) {
+
+        String s = "Bom Dia X";
+        s = s.replace("X", "Senhor");
+        s = s.concat("!");
+        s = s.toUpperCase();
+
+        System.out.println(s);
+
+        String x = "Bruno".toUpperCase();
+        System.out.println(x);
+
+        String y = "Bom dia X".replace("X", "Bruno").toUpperCase().concat("!");
+        System.out.println(y);
+
+        // Os tipos primitivos não possuem o operador "."
+        int a = 3;
+        System.out.println(a);
+    }
+}
---
+++ src/Fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package Fundamentos;
+
+/**
+ * @author Administrator
+ */
+
+public class PrimeiroPrograma {
+    /**
+     * ...
+     * @param args
+     */
+    public static void main(String[] args) {
+        //  Teste
+        System.out.println("Teste 1");
+        /*
+         *Teste
+         */
+        System.out.println("Teste 2");
+        System.out.println("Teste 3");
+    }
+}
