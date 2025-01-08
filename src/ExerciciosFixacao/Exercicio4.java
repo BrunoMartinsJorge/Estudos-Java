ad274837a58bd9e6f16649d1215e41b1e2fd000b
Bruno <2411550255@fema.edu.br>
2025-01-08 13:40:38 -0300

    Lista de exercicios! Operadores e notação ponto.

---
+++ src/ExerciciosFixacao/Exercicio4.java
@@ -0,0 +1,18 @@
+package ExerciciosFixacao;
+
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+        int variavel1 = 1;
+        variavel1++;
+        variavel1--;
+
+        Double variavel2 = 5.8291;
+        System.out.println(Math.round(variavel2));
+
+        String variavel3 = "Boa Tarde!";
+        System.out.println(variavel3.length() + " caracteres");
+        System.out.println(variavel3.toLowerCase());
+        System.out.println(variavel3.replace("!","#"));
+    }
+}
