5f42ba5c9359439de26c7dade0780a84022d9ffd
Bruno <2411550255@fema.edu.br>
2025-01-08 16:55:27 -0300

    Feat: Operadores de Atribuição, Ternarios, Relacionais, Unitarios e Exercicio de fixação

---
+++ src/Fundamentos/Operadores/OperadoresRelacionais.java
@@ -0,0 +1,42 @@
+package Fundamentos.Operadores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+
+        Locale.setDefault(new Locale("en", "US"));
+
+        Scanner sc = new Scanner(System.in);
+/*
+        int a = 97;
+        int b = 82;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+*/
+        System.out.println("Digite a média do aluno(a): ");
+        double media = sc.nextDouble();
+        System.out.println("Digite a frequencia do aluno(a) (Sem o simbolo de %): ");
+        double frequencia = sc.nextDouble();
+
+        if (media >= 7 && frequencia >= 75) {
+            System.out.println("Passou!");
+        } else if (media >= 4 && frequencia >= 75) {
+            System.out.println("Exame!");
+            System.out.println("Digite a nota que o aluno(a) tirou no exame: ");
+            double nota = sc.nextDouble();
+            if (nota + media == 10) {
+                System.out.println("Passou com exame!");
+            } else {
+                System.out.println("Não passou com o exame!");
+            }
+        } else {
+            System.out.println("Não passou!");
+        }
+    }
+}
---
+++ src/Fundamentos/Operadores/Ternarios.java
@@ -0,0 +1,11 @@
+package Fundamentos.Operadores;
+
+public class Ternarios {
+
+    public static void main(String[] args) {
+
+        double media = 4.5;
+        String resultado = media >= 7.0 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
