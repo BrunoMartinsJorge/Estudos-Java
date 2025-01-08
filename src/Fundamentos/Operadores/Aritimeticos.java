509e2b70d865013e97041110b633cfb3ba709778
Bruno <2411550255@fema.edu.br>
2025-01-08 14:53:50 -0300

    Feat: Ordenar os arquivos. Arquivos remetentes á tipos de operadores devem ser guardados na pasta "Operadores".

---
+++ src/Fundamentos/Operadores/Aritimeticos.java
@@ -0,0 +1,27 @@
+package Fundamentos.Operadores;
+
+public class Aritimeticos {
+
+    public static void main(String[] args) {
+
+        var x = 23.92;
+        double y = 92.1;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        int a = 2;
+        int b = 9;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a / (double) b);
+        System.out.println(a / (float) b);
+        System.out.println(a % b);
+
+    }
+}
