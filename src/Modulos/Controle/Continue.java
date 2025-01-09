833c0d557d257171597094b00bcad7415a948098
Bruno <2411550255@fema.edu.br>
2025-01-09 16:00:11 -0300

    Feat: Switch: Como usar switch em Java, com break e sem break

---
+++ src/Modulos/Controle/Continue.java
@@ -0,0 +1,16 @@
+package Modulos.Controle;
+
+public class Continue {
+
+    public static void main(String[] args) {
+
+        // Basicamente quando um continue é encontrado, ele pausa a repetição, mas somente a repetição em
+        // que ele foi encontrado, depois ele volta ao normal até acontecer novamente, como no exemplo abaixo:
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 1) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
