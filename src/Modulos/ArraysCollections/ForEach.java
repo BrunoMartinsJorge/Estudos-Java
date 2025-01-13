33336cb2ccf1e4aa2894ed18550d6b8404a9e016
Bruno <2411550255@fema.edu.br>
2025-01-13 16:36:10 -0300

    Feat: Exemplo de ForEach em Java

---
+++ src/Modulos/ArraysCollections/ForEach.java
@@ -0,0 +1,13 @@
+package Modulos.ArraysCollections;
+
+public class ForEach {
+
+    public static void main(String[] args) {
+
+        double[] notas = {10, 8.9, 7.95, 9};
+        // Foreach: Como é feito em Java abaixo! Repete de acordo com o número de elementos do array!
+        for (double nota : notas) {
+            System.out.println(nota);
+        }
+    }
+}
