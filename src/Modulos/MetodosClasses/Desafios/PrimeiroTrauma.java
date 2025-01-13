d3771fa9c756aee13da88d00d1e5729d6d763762
Bruno <2411550255@fema.edu.br>
2025-01-13 13:30:54 -0300

    Feat: Exercicio trauma Deu meio errado, pois o auto corretor arrumou o código antes que eu pode-se fazer algo.

---
+++ src/Modulos/MetodosClasses/Desafios/PrimeiroTrauma.java
@@ -0,0 +1,13 @@
+package Modulos.MetodosClasses.Desafios;
+
+public class PrimeiroTrauma {
+
+    static int a = 3;
+    int b = 2;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        int b = new PrimeiroTrauma().b;
+        System.out.println(b);
+    }
+}
