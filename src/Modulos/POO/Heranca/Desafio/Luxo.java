87e984fbb7c36a641a9889664f72209c9c6cf44a
Bruno <2411550255@fema.edu.br>
2025-01-17 13:50:11 -0300

    Fix: Default Metodo de como não ser necessário adicionar um metodo em uma classe que implementa uma interface.

--- src/Modulos/POO/Heranca/Desafio/Luxo.java
+++ src/Modulos/POO/Heranca/Desafio/Luxo.java
@@ -3,5 +3,11 @@ package Modulos.POO.Heranca.Desafio;
 public interface Luxo {
 
     void ligarAr();
+
     void desligarAr();
+
+    // Assim podemos fazer com que não exista a necessidade ultilizar forçadamente um metodo, podemos só adicionar um defalt e um return de 1(Caso o tipo seja int!).
+    default int velocidadeDoAr() {
+        return 1;
+    }
 }
